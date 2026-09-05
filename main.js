2a74d61bb74de2a48349dd5b2bdba8f99072b2bb
wordlyapp <devisserrik2@gmail.com>
2026-09-05 19:30:55 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        minWidth: 900,
+        minHeight: 600,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true
+        }
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
